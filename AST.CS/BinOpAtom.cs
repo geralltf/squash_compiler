e7ecdda1f8d7e06ec9c931c1998871f63fed9ab3
Gerallt Franke <geralltf@gmail.com>
2025-02-20 07:37:48 +1100

    Initial Commit.

---
+++ AST.CS/BinOpAtom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AST.CS
+{
+    public class BinOpAtom
+    {
+        public string operandValueLeft;
+        public string operandValueRight;
+
+        public BinOpAtom operandLeft; 
+        public BinOpAtom operandRight;
+
+        public OperationType operationType;
+
+        public enum OperationType
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide,
+            XOR,
+            Undefined
+        }
+    }
+}
---
+++ AST.CS/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AST.CS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wello, Horld!");
+
+            List<AST> computeResult = AST.Expr("int a = 2 / 8 * (9 + 2) + 3+1;");
+
+
+        }
+    }
+}
+
