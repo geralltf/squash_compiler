c963f8ddec0b39b2082472d8e7a5f333069878f5
Gerallt Franke <geralltf@gmail.com>
2025-04-21 20:40:51 +1000

    Example programs.

---
+++ ExampleCPrograms/EntryPoint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int a;a=6*5;
+	int b=1;b=2*3;
+	int c=1;c=3*5;
+	int e=1;e=4*3+1;
+	int d=1;d=3*8+4;
+
+	printf("%s", "Hello World!");
+	printf("%d", d);
+
+	return EXIT_SUCCESS;
+}
