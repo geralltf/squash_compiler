8e69f26718eec14fdf12d07aca4da5118b5480fe
Gerallt Franke <geralltf@gmail.com>
2025-04-23 12:25:28 +1000

    Test variable with expression.

--- AST.CS/scripts/EntryPoint.c
+++ AST.CS/scripts/EntryPoint.c
@@ -1,1 +1,1 @@
-int main(void){int a;a=6*5;int b=1;b=2*3;int c=1;c=3*5;int e=1;e=4*3+1;int d=1;d=3*8+4+e;return d;}
+int main(void){int a;a=6*5;int b=1;b=2*3;int c=1;c=3*5;int e=1;e=4*3+1;int d=1;d=3*8+4+e;return d*2+1;}
