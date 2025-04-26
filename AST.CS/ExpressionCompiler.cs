28340f88e46cee242ede74d9728537a4db957480
Gerallt Franke <geralltf@gmail.com>
2025-04-26 20:55:17 +1000

    Code minification pass before compilation stripping out whitespace.

--- AST.CS/ExpressionCompiler.cs
+++ AST.CS/ExpressionCompiler.cs
@@ -16,6 +16,7 @@ namespace SquashC.Compiler
 
         public ExpressionCompiler(string input)
         {
+            input = this.minifyCode(input);
             this.lexer = new Lexer(input);
             this.currentToken = lexer.GetNextToken();
             this.symbolTable = new SymbolTable();
@@ -26,6 +27,189 @@ namespace SquashC.Compiler
             //this.symbolTable.DefineVariable(VarType.Int, "a", 0);
         }
 
+        /// <summary>
+        /// Cleans up the code ready for the compiler to work with by removing new lines, 
+        /// tabs, spaces, and all whitespace.
+        /// 
+        /// Minifies the input string so to make it easier (and make it more efficient) 
+        /// for the compiler to parse and compile.
+        /// 
+        /// TODO: Strip out single line comments and multi line comments.
+        /// </summary>
+        private string minifyCode(string input)
+        {
+            string result = string.Empty;
+            int currentPos;
+            char currentChar = '\0';
+            char currentChar1 = '\0';
+            char currentChar2 = '\0';
+            char currentChar3 = '\0';
+            char currentChar4 = '\0';
+            char currentChar5 = '\0';
+            char currentChar6 = '\0';
+            char currentChar7 = '\0';
+            char currentChar8 = '\0';
+            char currentChar9 = '\0';
+
+            for (currentPos = 0; currentPos < input.Length;)
+            {
+                currentChar = input[currentPos];
+                if (currentPos + 1 < input.Length)
+                {
+                    currentChar1 = input[currentPos + 1];
+                }
+                else
+                {
+                    currentChar1 = '\0';
+                }
+                if (currentPos + 2 < input.Length)
+                {
+                    currentChar2 = input[currentPos + 2];
+                }
+                else
+                {
+                    currentChar2 = '\0';
+                }
+                if (currentPos + 3 < input.Length)
+                {
+                    currentChar3 = input[currentPos + 3];
+                }
+                else
+                {
+                    currentChar3 = '\0';
+                }
+                if (currentPos + 4 < input.Length)
+                {
+                    currentChar4 = input[currentPos + 4];
+                }
+                else
+                {
+                    currentChar4 = '\0';
+                }
+                if (currentPos + 5 < input.Length)
+                {
+                    currentChar5 = input[currentPos + 5];
+                }
+                else
+                {
+                    currentChar5 = '\0';
+                }
+                if (currentPos + 6 < input.Length)
+                {
+                    currentChar6 = input[currentPos + 6];
+                }
+                else
+                {
+                    currentChar6 = '\0';
+                }
+                if (currentPos + 7 < input.Length)
+                {
+                    currentChar7 = input[currentPos + 7];
+                }
+                else
+                {
+                    currentChar7 = '\0';
+                }
+                if (currentPos + 8 < input.Length)
+                {
+                    currentChar8 = input[currentPos + 8];
+                }
+                else
+                {
+                    currentChar8 = '\0';
+                }
+                if (currentPos + 9 < input.Length)
+                {
+                    currentChar9 = input[currentPos + 9];
+                }
+                else
+                {
+                    currentChar9 = '\0';
+                }
+
+                //TODO: Strip out any single line comments and strip out any multiline comments.
+                //if(currentChar == '\\' && currentChar1 == '\\')
+                //{
+
+                //}
+                if (char.IsWhiteSpace(currentChar) || currentChar == ' ' || currentChar == '\t' || currentChar == '\n' || currentChar == '\r')
+                {
+                    // Strip any whitespace except when it is neded to delimit a keyword.
+                    currentPos++;
+                }
+                else if (currentChar == 'v' && currentChar1 == 'a' && currentChar2 == 'r')
+                {
+                    result += currentChar;
+                    result += currentChar1;
+                    result += currentChar2;
+                    currentPos += 3;
+
+                    result += ' ';
+                    currentPos++;
+
+                }
+                else if (currentChar == 'i' && currentChar1 == 'n' && currentChar2 == 't')
+                {
+                    result += currentChar;
+                    result += currentChar1;
+                    result += currentChar2;
+                    currentPos += 3;
+
+                    result += ' ';
+                    currentPos++;
+
+                }
+                else if (currentChar == 'd' && currentChar1 == 'o' && currentChar2 == 'u' && currentChar3 == 'b' && currentChar4 == 'l' && currentChar5 == 'e')
+                {
+                    result += currentChar;
+                    result += currentChar1;
+                    result += currentChar2;
+                    result += currentChar3;
+                    result += currentChar4;
+                    result += currentChar5;
+                    currentPos += 6;
+
+                    result += ' ';
+                    currentPos++;
+
+                }
+                else if (currentChar == 's' && currentChar1 == 't' && currentChar2 == 'r' && currentChar3 == 'i' && currentChar4 == 'n' && currentChar5 == 'g')
+                {
+                    result += currentChar;
+                    result += currentChar1;
+                    result += currentChar2;
+                    result += currentChar3;
+                    result += currentChar4;
+                    result += currentChar5;
+                    currentPos += 6;
+
+                    result += ' ';
+                    currentPos++;
+
+                }
+                else if (currentChar == 'r' && currentChar1 == 'e' && currentChar2 == 't' && currentChar3 == 'u' && currentChar4 == 'r' && currentChar5 == 'n')
+                {
+                    result += currentChar;
+                    result += currentChar1;
+                    result += currentChar2;
+                    result += currentChar3;
+                    result += currentChar4;
+                    result += currentChar5;
+                    currentPos += 6;
+
+                    result += ' ';
+                    currentPos++;
+
+                }
+                else
+                {
+                    result += currentChar;
+                    currentPos++;
+                }
+            }
+
+            return result;
+        }
 
         public void CompileExpression()
         {
--- AST.CS/scripts/EntryPoint.c
+++ AST.CS/scripts/EntryPoint.c
@@ -1,1 +1,18 @@
-int sin(int val,int y){int A=2;return 1+3+val*y*A;}int main(void){int a;a=6*5;int b=1;b=2*3;int c=1;c=3*5;int e=1;e=4*3+1;int d=1;d=3*8+4+e;a=10+(b*5);return sin(a*c,b*a)*b;}
+double tan(double input){
+	return 0.7*input;
+}
+double cos(double val2){
+	return 0.2*val2;
+}
+int sin(int val,int y){
+	int A=2;
+	return 1+3+val*y*A;
+}
+int main(void){
+	int a;a=6*5;
+	int b=1;b=2*3;
+	int c=1;c=3*5;
+	int e=1;e=4*3+1;
+	int d=1;d=3*8+4+e;a=10+(b*5);
+	return sin(a*c,b*a)*b;
+}
