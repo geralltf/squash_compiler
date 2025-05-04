7b2966071e8447faf70b50c846888985319182df
Gerallt Franke <geralltf@gmail.com>
2025-05-04 22:16:15 +1000

    Cleanup of unused code. See tests in scripts folder.

--- AST.CS/Program.cs
+++ AST.CS/Program.cs
@@ -10,19 +10,7 @@ namespace AST.CS
 
             Console.WriteLine("*-*-*-*-*-*-* SquashC Compiler *-*-*-*-*-*-*");
 
-            //string code = "var x=sin(1)+3*9+(2*8)+(5*6)";
-            //string code = "a=sin(92,910,99)+3*9+(4*8)+(5*6)";
-            //string code = "double x = 4.3;";
-            //string code = "double a;\na=6.3;a=4*2";
-            //string code = "double a;a=6.3*5;double b=1;b=a*3;double c=a*5;double e=b*c+1;";
-            //string code = "int main(void){"
-            //                + "double a;a=6.3*5;double b=1;b=a*3;double c=a*5;double e=b*c+1;"
-            //                + "return 0+1+2+e;"
-            //                + "}";
             string code = System.IO.File.ReadAllText("./../../../scripts/EntryPoint.c");
-            // int main(void){double a;a=6.3*5;double b=1;b=a*3;double c=a*5;double e=b*c+1;return 0+1+2+3+e;}
-            // int main(void){int a;a=6*5;int b=1;b=a*3;int c=a*5;int e=b*c+1;return 0+1+2+3+e;}
-            //+ "while(true){break;}";
 
             SquashCompiler compiler = new SquashCompiler(code);
             compiler.CompileExpression();
