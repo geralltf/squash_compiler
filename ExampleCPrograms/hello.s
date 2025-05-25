5861bcf693886c9de3eb39bdaea5ca09a42fcc22
Gerallt Franke <geralltf@gmail.com>
2025-05-25 19:56:33 +1000

    Test compile to assembly with -S argument specifier using gcc to compile

---
+++ ExampleCPrograms/hello.s
@@ -0,0 +1,38 @@
+	.file	"hello.c"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello, Windows!\0"
+.LC1:
+	.ascii "%d\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$4430, -4(%rbp)
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	puts
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 14.2.0"
+	.def	puts;	.scl	2;	.type	32;	.endef
+	.def	printf;	.scl	2;	.type	32;	.endef
