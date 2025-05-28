f38cbb15bf84d2c2b55bc1ed4cc9878d8bda21e2
Gerallt Franke <geralltf@gmail.com>
2025-05-28 19:37:11 +1000

    Improved optimiser traversing AST with precedence already done prior.

--- AST.CS/Optimiser.cs
+++ AST.CS/Optimiser.cs
@@ -14,100 +14,7 @@ namespace SquashC.Compiler
     /// </summary>
     public class Optimiser
     {
-        private static Dictionary<string, int> precedence = new Dictionary<string, int>
-        {
-            {"*", 2},
-            {"/", 2},
-            {"+", 1},
-            {"-", 1}
-        };
-
-        private static Dictionary<string, bool> associativity = new Dictionary<string, bool>
-        {
-            {"*", true},
-            {"/", true},
-            {"+", true},
-            {"-", true}
-        };
-
-
-        private static void ParseNumber(string numberValue, out object val)
-        { 
-            int resultInt;
-            double resultDouble;
-            float resultFloat;
-
-            if (int.TryParse(numberValue, out resultInt))
-            {
-                val = resultInt;
-                return;
-            }
-            if (double.TryParse(numberValue, out resultDouble))
-            {
-                val = resultDouble;
-                return;
-            }
-            if (float.TryParse(numberValue, out resultFloat))
-            {
-                val = resultFloat;
-                return;
-            }
-            val = null;
-            Logger.Log.LogError("Optimiser.ParseNumber(): Unexpected type specified of number.");
-        }
-
-        private static int GetPrecedence(string op)
-        {
-            switch (op)
-            {
-                case "+":
-                case "-":
-                    return 1;
-                case "*":
-                case "/":
-                    return 2;
-                default:
-                    return 0;
-            }
-        }
-
-        private static bool ShouldEvaluateFirst(string op1, string op2)
-        {
-            if (!precedence.ContainsKey(op1) || !precedence.ContainsKey(op2))
-            {
-                return false;
-            }
-
-            if (precedence[op1] > precedence[op2])
-            {
-                return true;
-            }
-            else if (precedence[op1] == precedence[op2])
-            {
-                return associativity[op1];
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private static void CollapseNode(ref ASTNode node, string result)
-        {
-            // COLLAPSE.
-
-            // Change node value to new value from collapsed children.
-
-            node.Value = result;
-
-            // Detach node children and change node type to collapsed number.
-            node.Left = null;
-            node.Right = null;
-            node.Type = ASTNodeType.Number;
-        }
-
-
-        public static int OptimiseNode(ref ASTNode node)
+        public static object? OptimiseNode(ref ASTNode node)
         {
             ASTNode left = null;
             ASTNode right = null;
@@ -125,11 +32,11 @@ namespace SquashC.Compiler
 
             if (node.Type == ASTNodeType.Number)
             {
-                return int.Parse(node.Value);
+                return ParseNumber(node);
             }
 
-            int operandLeft = 0;
-            int operandRight = 0;
+            object? operandLeft = null;
+            object? operandRight = null;
 
             left = node.Left;
             right = node.Right;
@@ -157,38 +64,245 @@ namespace SquashC.Compiler
                 }
             }
 
-            int result = 0;
+            string result = string.Empty;
 
             if (node.Type == ASTNodeType.BIN_OP)
             {
-                switch (node.Value)
-                {
-                    case "+":
-                        result = operandLeft + operandRight;
-                        break;
-                    case "-":
-                        result = operandLeft - operandRight;
-                        break;
-                    case "*":
-                        result = operandLeft * operandRight;
-                        break;
-                    case "/":
-                        result = operandLeft / operandRight;
-                        break;
-                    default:
-                        Logger.Log.LogError("Optimiser.OptimiseNode(): Invalid operator");
-                        break;
-                }
+                result = ApplyOperator(node, operandLeft, operandRight);
 
-                // COLLAPSE.
-
-                // Change node value to new value from collapsed children.
-                node.Value = result.ToString();
-                node.Left = null;
-                node.Right = null;
-                node.Type = ASTNodeType.Number;
+                CollapseNode(ref node, result);
             }
 
+            return null;
+        }
+
+        private static void CollapseNode(ref ASTNode node, string result)
+        {
+            // COLLAPSE.
+
+            // Change node value to new value from collapsed children.
+            node.Value = result;
+            node.Left = null;
+            node.Right = null;
+            node.Type = ASTNodeType.Number;
+        }
+
+        private static string ApplyOperator(ASTNode node, object operandLeft, object operandRight)
+        {
+            string result = string.Empty;
+            if (operandLeft is int)
+            {
+                int resultLeftInt = (int)operandLeft;
+                if (operandRight is int)
+                {
+                    int resultRightInt = (int)operandRight;
+
+                    result = ApplyOperator(node, resultLeftInt, resultRightInt).ToString();
+                }
+                else if (operandRight is double)
+                {
+                    double resultRightDouble = (double)operandRight;
+
+                    result = ApplyOperator(node, resultLeftInt, resultRightDouble).ToString();
+                }
+                else if (operandRight is float)
+                {
+                    float resultRightFloat = (float)operandRight;
+
+                    result = ApplyOperator(node, resultLeftInt, resultRightFloat).ToString();
+                }
+            }
+            else if (operandLeft is double)
+            {
+                double resultLeftDouble = (double)operandLeft;
+                if (operandRight is int)
+                {
+                    int resultRightInt = (int)operandRight;
+
+                    result = ApplyOperator(node, resultLeftDouble, resultRightInt).ToString();
+                }
+                else if (operandRight is double)
+                {
+                    double resultRightDouble = (double)operandRight;
+
+                    result = ApplyOperator(node, resultLeftDouble, resultRightDouble).ToString();
+                }
+                else if (operandRight is float)
+                {
+                    float resultRightFloat = (float)operandRight;
+
+                    result = ApplyOperator(node, resultLeftDouble, resultRightFloat).ToString();
+                }
+            }
+            else if (operandLeft is float)
+            {
+                double resultLeftFloat = (float)operandLeft;
+                if (operandRight is int)
+                {
+                    int resultRightInt = (int)operandRight;
+
+                    result = ApplyOperator(node, resultLeftFloat, resultRightInt).ToString();
+                }
+                else if (operandRight is double)
+                {
+                    double resultRightDouble = (double)operandRight;
+
+                    result = ApplyOperator(node, resultLeftFloat, resultRightDouble).ToString();
+                }
+                else if (operandRight is float)
+                {
+                    float resultRightFloat = (float)operandRight;
+
+                    result = ApplyOperator(node, resultLeftFloat, resultRightFloat).ToString();
+                }
+            }
+            return result;
+        }
+
+        private static object? ParseNumber(ASTNode node)
+        {
+            int resultInt;
+            double resultDouble;
+            float resultFloat;
+
+            if (int.TryParse(node.Value, out resultInt))
+            {
+                return resultInt;
+            }
+            else
+            {
+                if (double.TryParse(node.Value, out resultDouble))
+                {
+                    return resultDouble;
+                }
+                else
+                {
+                    if (float.TryParse(node.Value, out resultFloat))
+                    {
+                        return resultFloat;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static int ApplyOperator(ASTNode node, int operandLeft, int operandRight)
+        {
+            int result = 0;
+            switch (node.Value)
+            {
+                case "+":
+                    result = operandLeft + operandRight;
+                    break;
+                case "-":
+                    result = operandLeft - operandRight;
+                    break;
+                case "*":
+                    result = operandLeft * operandRight;
+                    break;
+                case "/":
+                    result = operandLeft / operandRight;
+                    break;
+                default:
+                    Logger.Log.LogError("Optimiser.OptimiseNode(): Invalid operator");
+                    break;
+            }
+            return result;
+        }
+
+        private static double ApplyOperator(ASTNode node, double operandLeft, int operandRight)
+        {
+            double result = 0;
+            switch (node.Value)
+            {
+                case "+":
+                    result = operandLeft + operandRight;
+                    break;
+                case "-":
+                    result = operandLeft - operandRight;
+                    break;
+                case "*":
+                    result = operandLeft * operandRight;
+                    break;
+                case "/":
+                    result = operandLeft / operandRight;
+                    break;
+                default:
+                    Logger.Log.LogError("Optimiser.OptimiseNode(): Invalid operator");
+                    break;
+            }
+            return result;
+        }
+
+        private static double ApplyOperator(ASTNode node, double operandLeft, double operandRight)
+        {
+            double result = 0;
+            switch (node.Value)
+            {
+                case "+":
+                    result = operandLeft + operandRight;
+                    break;
+                case "-":
+                    result = operandLeft - operandRight;
+                    break;
+                case "*":
+                    result = operandLeft * operandRight;
+                    break;
+                case "/":
+                    result = operandLeft / operandRight;
+                    break;
+                default:
+                    Logger.Log.LogError("Optimiser.OptimiseNode(): Invalid operator");
+                    break;
+            }
+            return result;
+        }
+
+        private static double ApplyOperator(ASTNode node, double operandLeft, float operandRight)
+        {
+            double result = 0;
+            switch (node.Value)
+            {
+                case "+":
+                    result = operandLeft + operandRight;
+                    break;
+                case "-":
+                    result = operandLeft - operandRight;
+                    break;
+                case "*":
+                    result = operandLeft * operandRight;
+                    break;
+                case "/":
+                    result = operandLeft / operandRight;
+                    break;
+                default:
+                    Logger.Log.LogError("Optimiser.OptimiseNode(): Invalid operator");
+                    break;
+            }
+            return result;
+        }
+
+        private static float ApplyOperator(ASTNode node, float operandLeft, float operandRight)
+        {
+            float result = 0;
+            switch (node.Value)
+            {
+                case "+":
+                    result = operandLeft + operandRight;
+                    break;
+                case "-":
+                    result = operandLeft - operandRight;
+                    break;
+                case "*":
+                    result = operandLeft * operandRight;
+                    break;
+                case "/":
+                    result = operandLeft / operandRight;
+                    break;
+                default:
+                    Logger.Log.LogError("Optimiser.OptimiseNode(): Invalid operator");
+                    break;
+            }
             return result;
         }
     }
