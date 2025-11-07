90ebeac1d4b3c01c120ecb412dd2edff9f8d0934
Gerallt Franke <geralltf@gmail.com>
2025-11-07 20:20:30 +1100

    Correct way to position the Encoder struct so all the parts of it work.

--- squash/Instruction.Create.h
+++ squash/Instruction.Create.h
@@ -817,6 +817,81 @@ struct ZeroBytesHandler
 	struct OpCodeHandler* base;
 };
 
+unsigned char SegmentOverrides[6] =
+{
+	0x26,
+	0x2E,
+	0x36,
+	0x3E,
+	0x64,
+	0x65
+};
+
+unsigned int s_immSizes[19] =
+{
+	0,// None
+	1,// Size1
+	2,// Size2
+	4,// Size4
+	8,// Size8
+	3,// Size2_1
+	2,// Size1_1
+	4,// Size2_2
+	6,// Size4_2
+	1,// RipRelSize1_Target16
+	1,// RipRelSize1_Target32
+	1,// RipRelSize1_Target64
+	2,// RipRelSize2_Target16
+	2,// RipRelSize2_Target32
+	2,// RipRelSize2_Target64
+	4,// RipRelSize4_Target32
+	4,// RipRelSize4_Target64
+	1,// SizeIbReg
+	1,// Size1OpCode
+};
+
+const char* ERROR_ONLY_1632_BIT_MODE = "The instruction can only be used in 16/32-bit mode";
+const char* ERROR_ONLY_64_BIT_MODE = "The instruction can only be used in 64-bit mode";
+
+struct Encoder
+{
+	unsigned int Internal_PreventVEX2;
+	unsigned int Internal_VEX_WIG_LIG;
+	unsigned int Internal_VEX_LIG;
+	unsigned int Internal_EVEX_WIG;
+	unsigned int Internal_EVEX_LIG;
+	unsigned int Internal_MVEX_WIG;
+
+	//readonly CodeWriter writer;
+	int bitness;
+	//OpCodeHandler[] handlers;
+	//unsigned int immSizes[19];
+	unsigned long currentRip;
+	char* errorMessage;
+	struct OpCodeHandler* handler;
+	unsigned int eip;
+	unsigned int displAddr;
+	unsigned int immAddr;
+	unsigned int Immediate;
+	// high 32 bits if it's a 64-bit immediate
+	// high 32 bits if it's an IP relative immediate (jcc,call target)
+	// high 32 bits if it's a 64-bit absolute address
+	unsigned int ImmediateHi;
+	unsigned int Displ;
+	// high 32 bits if it's an IP relative mem displ (target)
+	unsigned int DisplHi;
+	enum EncoderFlags opSize16Flags;
+	enum EncoderFlags opSize32Flags;
+	enum EncoderFlags adrSize16Flags;
+	enum EncoderFlags adrSize32Flags;
+	unsigned int OpCode;
+	enum EncoderFlags EncoderFlags;
+	enum DisplSize DisplSize;
+	enum ImmSize ImmSize;
+	unsigned char ModRM;
+	unsigned char Sib;
+};
+
 enum EncodingKind GetEncodingKindByOpcode(enum Code opcode);
 
 void OpCodeHandler_init(struct OpCodeHandler** o, 
@@ -1233,71 +1308,6 @@ enum CodeSize
 	CodeSize_Code32 = 2,
 	/// <summary>64-bit code</summary>
 	CodeSize_Code64 = 3,
-};
-
-unsigned int s_immSizes[19] =
-{
-	0,// None
-	1,// Size1
-	2,// Size2
-	4,// Size4
-	8,// Size8
-	3,// Size2_1
-	2,// Size1_1
-	4,// Size2_2
-	6,// Size4_2
-	1,// RipRelSize1_Target16
-	1,// RipRelSize1_Target32
-	1,// RipRelSize1_Target64
-	2,// RipRelSize2_Target16
-	2,// RipRelSize2_Target32
-	2,// RipRelSize2_Target64
-	4,// RipRelSize4_Target32
-	4,// RipRelSize4_Target64
-	1,// SizeIbReg
-	1,// Size1OpCode
-};
-
-const char* ERROR_ONLY_1632_BIT_MODE = "The instruction can only be used in 16/32-bit mode";
-const char* ERROR_ONLY_64_BIT_MODE = "The instruction can only be used in 64-bit mode";
-
-struct Encoder
-{
-	unsigned int Internal_PreventVEX2;
-	unsigned int Internal_VEX_WIG_LIG;
-	unsigned int Internal_VEX_LIG;
-	unsigned int Internal_EVEX_WIG;
-	unsigned int Internal_EVEX_LIG;
-	unsigned int Internal_MVEX_WIG;
-
-	//readonly CodeWriter writer;
-	int bitness;
-	//OpCodeHandler[] handlers;
-	//unsigned int immSizes[19];
-	unsigned long currentRip;
-	char* errorMessage;
-	struct OpCodeHandler* handler;
-	unsigned int eip;
-	unsigned int displAddr;
-	unsigned int immAddr;
-	unsigned int Immediate;
-	// high 32 bits if it's a 64-bit immediate
-	// high 32 bits if it's an IP relative immediate (jcc,call target)
-	// high 32 bits if it's a 64-bit absolute address
-	unsigned int ImmediateHi;
-	unsigned int Displ;
-	// high 32 bits if it's an IP relative mem displ (target)
-	unsigned int DisplHi;
-	enum EncoderFlags opSize16Flags;
-	enum EncoderFlags opSize32Flags;
-	enum EncoderFlags adrSize16Flags;
-	enum EncoderFlags adrSize32Flags;
-	unsigned int OpCode;
-	enum EncoderFlags EncoderFlags;
-	enum DisplSize DisplSize;
-	enum ImmSize ImmSize;
-	unsigned char ModRM;
-	unsigned char Sib;
 };
 
 struct Encoder* Encoder_new()
