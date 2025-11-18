fc6d6aa5737b7f16e993d4652382c1e03ce3d252
Gerallt Franke <geralltf@gmail.com>
2025-11-18 18:33:23 +1100

    Fix to remove duplicate RoundingControl enum.

--- squash/Encoder.h
+++ squash/Encoder.h
@@ -24,20 +24,6 @@ enum PrefixFlags
 	PF_PreferEvex = 0x20,
 };
 
-enum RoundingControl 
-{
-	/// <summary>No rounding mode</summary>
-	RC_None = 0,
-	/// <summary>Round to nearest (even)</summary>
-	RC_RoundToNearest = 1,
-	/// <summary>Round down (toward -inf)</summary>
-	RC_RoundDown = 2,
-	/// <summary>Round up (toward +inf)</summary>
-	RC_RoundUp = 3,
-	/// <summary>Round toward zero (truncate)</summary>
-	RC_RoundTowardZero = 4,
-};
-
 enum AssemblerOperandFlags 
 {
 	/// <summary>
