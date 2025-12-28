a25de832803412e71be10b1b3889c87f9b80a617
Gerallt Franke <geralltf@gmail.com>
2025-12-28 17:09:12 +1100

    Simple hello world program.

--- ExampleCPrograms/EntryPoint.c
+++ ExampleCPrograms/EntryPoint.c
@@ -1,16 +1,8 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 int main(void)
 {
-	int a;a=6*5;
-	int b=1;b=2*3;
-	int c=1;c=3*5;
-	int e=1;e=4*3+1;
-	int d=1;d=3*8+4;
+	printf("%s", "Hello World!\n123");
 
-	printf("%s", "Hello World!");
-	printf("%d", d);
-
-	return EXIT_SUCCESS;
+	return 0;
 }
